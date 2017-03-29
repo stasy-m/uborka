a5f25c35676a66cc7f0cc1afa0548f7489265005
stasy-m <stasy.m.2013@gmail.com>
2017-03-29 10:38:39 +0300

    Create floors.py

---
+++ floors.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import urllib
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") != "задание":
+        return {}
+    result = req.get("result")
+    parameters = result.get("parameters")
+    floor = parameters.get("floor")
+
+    if floor > 0:
+        floor = "на " + str(floor) + " этаже"
+    else:
+        floor = "на " + str(floor) + " этажах"
+        
+    print("Response:")
+    print(floor)
+
+    return {
+        "floor": floor,
+        "source": "apiai-onlinestore-shipping"
+    }
+
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print "Starting app on port %d" % port
+
+app.run(debug=True, port=port, host='0.0.0.0')
